bccea094e8edf0f8a63bb6149f2729dd224ad4cf
joshuader6 <yoshi199234@gmail.com>
2019-11-04 07:20:54 +0100

    Update to use Config.ini

--- create.py
+++ create.py
@@ -1,18 +1,53 @@
 import sys
 import os
+from distutils.util import strtobool
+from pathlib import Path
+import configparser
 from github import Github
 
-path = "/Users/kalle/Documents/Projects/MyProjects/"
+def user_yes_no_query(question):
+    sys.stdout.write(f'{question} [y/n]\n')
+    while True:
+        try:
+            return strtobool(input().lower())
+        except ValueError:
+            sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
 
-username = "" #Insert your github username here
-password = "" #Insert your github password here
+
+
+def get_config()->configparser.ConfigParser:
+    """Provides Config object from Filesystem. if not exist, interactively create
+    """
+    config = configparser.ConfigParser()
+    cconfig_path = 'Config.ini'
+    if not config.has_section('GitHub'):
+        config.add_section('GitHub')
+    github =config['GitHub']
+    github['username'] = github.get('username',input('GitHub Username: '))
+
+    if not os.path.exists('config.ini'):
+        savesett = user_yes_no_query('Do you want to create settings (Plaintext)? No will use Interactive mode')
+        if not savesett:
+            always_interactive = user_yes_no_query('Never ask again?')
+    #! Add interactive wuestions for settings creation
+    return config
+
 
 def create():
+    """Create Github Repository
+    """
+    config = get_config()
+    username = config['GitHub']['username']
+    password = config['Github']['password']
+    path = config['Paths']['projectpath']
+
     folderName = str(sys.argv[1])
-    os.makedirs(path + str(folderName)
+    os.makedirs(path / folderName)
     user = Github(username, password).get_user()
+    #! Add wrong login Handler
     repo = user.create_repo(folderName)
-    print("Succesfully created repository {}".format(folderName))
+    print(f"Succesfully created repository {folderName}")
+    #! Return Clone URL for origin add
 
 if __name__ == "__main__":
     create()
