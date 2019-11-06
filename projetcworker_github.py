a200a83c8dbd2ffe46b8863237603a6a805a2896
joshuader6 <yoshi199234@gmail.com>
2019-11-06 19:39:50 +0100

    added project folder selector

--- projetcworker_github.py
+++ projetcworker_github.py
@@ -4,21 +4,28 @@ from pathlib import Path
 import github
 import config
 import subprocess
+import shutil
 from PyInquirer import prompt
 
 MYCONFIG = config.ConfigHandler('Config.ini')
 
 
-def github_clone(repo,Repository,target_folder:Path):
+def github_clone(repo,target_folder:Path):
     """Clones Github Repository to disk
     Arguments:
         repo {github.Repository.Repository} -- [Repo class]
         target_folder {Path} -- [where to? ]
     """
-    repoorigin = repo.clone_url()
-    print(f"Cloning from: {repoorigin}")
-    pr = subprocess.call(['git', 'clone ' , str(repoorigin),target_folder])
-    print(pr)
+    if os.path.exists(target_folder):
+        if config.simple_yes_no_query(f'The Target directory: "{target_folder} alredy exists. Overwrite?'):
+            shutil.rmtree(target_folder)
+        else:
+            print('Aborting Clone, Folder alredy exists!')
+    if not os.path.exists(target_folder):
+        repoorigin = repo.clone_url()
+        print(f"Cloning from: {repoorigin}")
+        pr = subprocess.call(['git', 'clone ' , str(repoorigin),target_folder])
+        print(pr)
 
 def github_login(username:str,password:str)->github.Github:
     alteredsetting =False
@@ -91,6 +98,26 @@ def select_github_repo(github_user):
     choice = answer['Repo']
     return github_user.get_repo(choice)
 
+def github_select_and_clone(github_user,project_path:Path):
+    myrepo = select_github_repo(github_user)
+    print(f'You seleced: {myrepo.name}')
+    target_path = project_path / myrepo.name
+    github_clone(myrepo,target_path)
+
+def project_select_folder(project_path:Path)->Path:
+    allfolders = [f.name for f in project_path.glob('*') if f.is_dir()]
+    allfolders.append('...Exit!')
+    q = [
+        {
+        'type': 'list',
+        'name': 'folder',
+        'message': 'Select project folder?',
+        'choices': allfolders,
+        }
+    ]
+    answer = project_path /  prompt(q)['folder']
+    if os.path.exists(answer):
+        return answer
 
 def main():
     """Main worker GitHub
@@ -105,10 +132,34 @@ def main():
 
     git = github_login(github_username,github_password)
     user = git.get_user()
-    if not user:
-        sys.exit()
-    myrepo = select_github_repo(user)
-    print(f'you seleced: {myrepo.name}')
+
+    q = [
+        {
+        'type': 'list',
+        'name': 'action',
+        'message': 'What to do?',
+        'choices': [
+            'Clone Repo from GitHub',
+            'Remove Local Repo',
+            'Exit!'],
+        }
+    ]
+
+    while True:
+        answer = prompt(q)['action']
+        if answer == 'Clone Repo from GitHub':
+            github_select_and_clone(user,path)
+            return
+        elif answer=='Exit!':
+            return
+        elif answer=='Remove Local Repo':
+            fol = project_select_folder(path)
+            print(f'selected: {fol}')
+            return
+        else:
+            print('Unsupported Function Selected')
+
+
 
 if __name__ == "__main__":
     main()
